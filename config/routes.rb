acdb8961fc7e0a018363cc251bb673e96367c11d
takuitose <132122425+takuitose@users.noreply.github.com>
2023-05-05 04:10:16 +0000

    [Finish]4-3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   get 'lists/new'
   post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
